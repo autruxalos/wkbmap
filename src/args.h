ed375e547ec61b7dccb92e2da7157fb31fdb54ea
autruxalos <rivasfrismanabeleduardo@gmail.com>
2026-08-24 15:38:44 -0400

    Update args.h

--- src/args.h
+++ src/args.h
@@ -1,1 +1,21 @@
+#ifndef WKBMAP_ARGS_H
+#define WKBMAP_ARGS_H
 
+typedef struct {
+    char *rules;
+    char *model;
+    char *layout;
+    char *variant;
+    char *options;
+
+    int query;
+    int help;
+    int version;
+} WkbConfig;
+
+int parse_args(int argc, char **argv, WkbConfig *config);
+void free_config(WkbConfig *config);
+void print_help(void);
+void print_version(void);
+
+#endif
