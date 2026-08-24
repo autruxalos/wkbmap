7091eaed313b999a6dd7cf679eadb3febd1c7cc8
autruxalos <rivasfrismanabeleduardo@gmail.com>
2026-08-24 15:40:42 -0400

    Update xkb.c

--- src/xkb.c
+++ src/xkb.c
@@ -1,8 +1,43 @@
-#ifndef WKBMAP_XKB_H
-#define WKBMAP_XKB_H
+#include <stdio.h>
 
-#include "args.h"
+#include <xkbcommon/xkbcommon.h>
 
-int xkb_validate(const WkbConfig *config);
+#include "xkb.h"
 
-#endif
+int xkb_validate(const WkbConfig *config)
+{
+    struct xkb_context *context;
+    struct xkb_keymap *keymap;
+
+    struct xkb_rule_names names = {
+        .rules = config->rules,
+        .model = config->model,
+        .layout = config->layout,
+        .variant = config->variant,
+        .options = config->options
+    };
+
+    context = xkb_context_new(XKB_CONTEXT_NO_FLAGS);
+
+    if (!context) {
+        fprintf(stderr, "wkbmap: failed to create XKB context\n");
+        return 1;
+    }
+
+    keymap = xkb_keymap_new_from_names(
+        context,
+        &names,
+        XKB_KEYMAP_COMPILE_NO_FLAGS
+    );
+
+    if (!keymap) {
+        fprintf(stderr, "wkbmap: failed to compile XKB keymap\n");
+        xkb_context_unref(context);
+        return 1;
+    }
+
+    xkb_keymap_unref(keymap);
+    xkb_context_unref(context);
+
+    return 0;
+}
